17522d77d5411f5f5a3a0433c8e4a25ce353afc5
Abdiel Carrasco <redskullcoder@gmail.com>
2022-08-28 15:38:11 -0700

    AVOCADO01 - BASE PROJECT

---
+++ lib/src/utils/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+
+  static Color primaryColorDark = Color(0xFF98BF14);
+  static Color primaryColor = Color(0xFF579700);
+  static Color primaryOpacityColor = Color.fromRGBO(43, 122, 11, 0.09);
+
+}
